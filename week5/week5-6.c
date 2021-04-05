ba84365b155e9677c3a847a40f8d29f93ab6ebf6
lmhstrar070 <lmhstrar070@naver.com>
2021-04-05 16:33:23 +0900

    .

--- week5/week5-6.c
+++ week5/week5-6.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
-   int a = 2;
-   int b = -3;
-   int c = 5;
-   int d = -7;
-   int e = 11;
-   printf("%d\n", 7+c*--d/e);
-   printf("%d\n", 2*a%-b+c+1);
-   printf("%d\n", 39/-++e-+29%c);
-   printf("%d\n\n", a+=b+=c+=1+2);
-   printf("%d\n",7+((c*(--d))/e));
-   printf("%d\n",(((2 * a % - b) + c) + 1));
-   printf("%d\n",(39 / (- (++ e))) - ((+ 29) % c));
-   printf("%d\n",a += (b += (c += (1 + 2))));
+   int a = 2, b = -3, c = 5, d = -7, e = 11;
 
-   printf("%d\n",c*--d/e);
-   printf("%d\n",((c*--d)/e));
+   printf("%d\n", 7 + c * --d / e);
+   printf("%d\n", 2 * a % - b + c + 1);
+   printf("%d\n", 39 / - ++ e - + 29 % c);
+   printf("%d\n\n", a += b += c += 1 + 2);
+   
+   a = 2, b = -3, c = 5, d = -7, e = 11;
+   printf("%d\n",7 + ((c * (--d)) / e));
+   printf("%d\n",(((2 * a) % (- b)) + c) + 1);
+   printf("%d\n",(39 / (- (++ e))) - ((+ 29) % c));
+   printf("%d\n",(a += (b += (c += (1 + 2)))));
+   
    return 0;
 }  
