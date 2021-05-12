d295dfb8bc84104ecc90c112ed8dbbc0099471aa
MinhyeongLee <lmhstrar070@naver.com>
2021-05-13 01:17:37 +0900

    .

--- com_math/bubble.c
+++ com_math/bubble.c
@@ -13,18 +13,18 @@ int main(void)
     //Ascending order
     while(1)
     {    
-        count=0;
+        count=0; //반복할 때마다 원소 순서 변경 횟수 카운트 초기화
         for(int i=0; i<9; i++)
         {
-          if(strcmp(animals[i], animals[i+1])>0)
+          if(strcmp(animals[i], animals[i+1])>0) //앞이 큰 경우 양수 리턴
             {
                 strcpy(save, animals[i]);
                 strcpy(animals[i], animals[i+1]);
                 strcpy(animals[i+1], save);
-                count++;
+                count++; //원소의 순서를 변경하면 카운트
             }
         }
-        if(count==0)
+        if(count==0) //원소의 순서를 변경하지 않으면(정렬이 완료되었으면) 반복문 탈출
             break;
     }
     printf("[Ascending order] : ");
@@ -39,18 +39,18 @@ int main(void)
     //Descending order
     while(1)
     {    
-        count=0;
+        count=0; //반복할 때마다 원소 순서 변경 횟수 카운트 초기화
         for(int i=0; i<9; i++)
         {
-          if(strcmp(animals[i], animals[i+1])<0)
+          if(strcmp(animals[i], animals[i+1])<0) //앞이 작은 경우 음수 리턴
             {
                 strcpy(save, animals[i]);
                 strcpy(animals[i], animals[i+1]);
                 strcpy(animals[i+1], save);
-                count++;
+                count++; //원소의 순서를 변경하면 카운트함
             }
         }
-        if(count==0)
+        if(count==0) //원소의 순서를 변경하지 않으면(정렬이 완료되었으면) 반복문 탈출
             break;
     }
     printf("[Descending order] : ");
