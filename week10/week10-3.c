e029b6e91536ab021e5d1443a9a07dc8e8db5f7a
MinhyeongLee <41287145+LeeMin-hyeong@users.noreply.github.com>
2021-05-18 12:36:33 +0900

    '1' is not a prime number

--- week10/week10-3.c
+++ week10/week10-3.c
@@ -3,15 +3,20 @@
 int is_prime(int n)
 {
     int count=0;
-    for(int i=1; i<=n; i++)
-    {
-        if(n%i==0)
-            count++;
-    }
-    if(count>2)
+    if(n==1)
         return 0;
     else
-        return 1;
+    {
+        for(int i=1; i<=n; i++)
+        {
+            if(n%i==0)
+                count++;
+        }
+        if(count>2)
+            return 0;
+        else
+            return 1;
+    }
 }
 
 int main(void)
