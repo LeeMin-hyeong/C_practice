6c4a27c2a636d3e93bd22c27a230c0539ec2df97
LeeMin-hyeong <lmhstrar070@naver.com>
2021-06-01 16:18:02 +0900

    .

---
+++ week14/20211435.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int lotto[5][6]={0};
+
+    srand(time(NULL));
+
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<6 j++)
+        {
+            lotto[i][j]=rand()%45+1
+            if(j>0)
+            {
+                
+            }
+        }
+    }
+    for(int i=0; i<5; i++)
+    {
+        
+    }
+
+}
